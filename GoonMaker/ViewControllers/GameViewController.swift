749b9900fa57d985c4f060cc62e852a19c899675
XDEric <eeeric3995@yahoo.com>
2021-02-14 15:48:32 -0500

    have bottom button animation completed

--- GoonMaker/ViewControllers/GameViewController.swift
+++ GoonMaker/ViewControllers/GameViewController.swift
@@ -20,37 +20,48 @@ class GameViewController: UIViewController {
     var slider2 = DSSlider()
     var slider3 = DSSlider()
     
+    var gameTimer: Timer?
+    
     @IBOutlet weak var buttonBoundary: UIView!
     @IBOutlet weak var shrnkButtonOutlet: UIButton!
     
     @IBAction func shrinkButton(_ sender: Any) {
+        revertAnimate()
     }
     
     func setupBtnBoundary(){
         buttonBoundary.layer.cornerRadius = buttonBoundary.frame.width / 2
         shrnkButtonOutlet.layer.cornerRadius = shrnkButtonOutlet.frame.width / 2
+        
     }
     
-    
-    
-//    var mytimer =  Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(timerAction), userInfo: nil, repeats: true)
-    
-    @objc func timerAction(){
-//            let Range =  slider0.maximumValue - slider0.minimumValue;
-//            let Increment = Range/100;
-//            let newval = slider0.value + Increment;
-//            if(Increment <= slider0.maximumValue)
-//            {
-//                slider0.setValue(newval, animated: true)
-//                print("The value of the slider is now \(slider0.value)")
-//                sliderValue = Int(slider0.value)
-//            }
-//            else if (Increment >= slider0.minimumValue)
-//            {
-//                slider0.setValue(newval, animated: true)
-//            }
+    @objc func animate(){
+        UIView.animateKeyframes(withDuration: 10,
+                                delay: 0.5,
+                                options: [.allowUserInteraction, .beginFromCurrentState]) {
+            self.shrnkButtonOutlet.transform = CGAffineTransform(scaleX: 5, y: 5)
+            UIView.addKeyframe(withRelativeStartTime: 0.5/4, relativeDuration: 1/4) {
+                self.shrnkButtonOutlet.backgroundColor = #colorLiteral(red: 0.9529411793, green: 0.6862745285, blue: 0.1333333403, alpha: 1)
+            }
+            UIView.addKeyframe(withRelativeStartTime: 2/4, relativeDuration: 1/4) {
+                self.shrnkButtonOutlet.backgroundColor = #colorLiteral(red: 0.9372549057, green: 0.3490196168, blue: 0.1921568662, alpha: 1)
+            }
+            UIView.addKeyframe(withRelativeStartTime: 3.5/4, relativeDuration: 1/4) {
+                self.shrnkButtonOutlet.backgroundColor = #colorLiteral(red: 0.5807225108, green: 0.066734083, blue: 0, alpha: 1)
+            }
         }
-    
+        
+    }
+    func revertAnimate(){
+        UIView.animate(withDuration: 0.2, delay: 0, options: .curveLinear) {
+            self.shrnkButtonOutlet.transform = .identity
+            self.shrnkButtonOutlet.backgroundColor = #colorLiteral(red: 0.2052684426, green: 0.7807833552, blue: 0.3487253785, alpha: 1)
+        } completion: { _ in
+            print("reverted")
+            self.animate()
+        }
+        
+    }
     
     //MARK: Lifecylce
     override func viewDidLoad() {
@@ -61,7 +72,9 @@ class GameViewController: UIViewController {
         setupSliders(slider: slider3, UIViewContainer: sliderContainer3)
         viewSetup()
         setupBtnBoundary()
-//        sliderHStackView.transform = CGAffineTransform(rotationAngle: .pi / 2)
+        animate()
+//        gameTimer =  Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(animate), userInfo: nil, repeats: true)
+        //        sliderHStackView.transform = CGAffineTransform(rotationAngle: .pi / 2)
         
     }
     
@@ -92,7 +105,7 @@ extension GameViewController {
         slider.isShowSliderText = true
         slider.isEnabled = true
         
-//        slider.transform = CGAffineTransform(rotationAngle: .pi/2)
+        //        slider.transform = CGAffineTransform(rotationAngle: .pi/2)
         
         slider.sliderAnimationVelocity = 0.2
         slider.sliderViewTopDistance = 0.0

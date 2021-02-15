12b7e127ffb644173f3aed2b8666308865d909b4
Neema Philippe <neemaphilippe@pursuit.org>
2021-02-15 17:33:27 -0500

    Added objects to leaderboard storyboard and swift file

--- GoonMaker/ViewControllers/LeaderboardViewController.swift
+++ GoonMaker/ViewControllers/LeaderboardViewController.swift
@@ -2,7 +2,7 @@
 //  LeaderboardViewController.swift
 //  GoonMaker
 //
-//  Created by Gregory Keeley on 2/12/21.
+//  Created by Neema on 2/15/21.
 //
 
 import UIKit
@@ -10,13 +10,34 @@ import UIKit
 
 class LeaderboardViewController: UIViewController {
     
-    @IBOutlet weak var leaderboardTableView: UITableView!
     
-
+    @IBOutlet weak var leaderboardTableView: UITableView!
+ 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-         
-
+        leaderboardTableView.delegate = self
+        leaderboardTableView.dataSource = self
+        view.backgroundColor = .systemTeal
+        
     }
-
+    
+}
+extension LeaderboardViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = leaderboardTableView.dequeueReusableCell(withIdentifier: "leaderboardCell", for: indexPath)
+//         cell.textLabel?.text = 
+//         cell.detailTextLabel?.text =
+        return cell
+        
+    }
+    
+    func tableView(_ tableView:UITableView, didSelectRowAt indexPath: IndexPath) {
+        let selectedRow = indexPath.row
+        print(selectedRow)
+    }
 }

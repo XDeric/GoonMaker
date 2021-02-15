bf16a5e667cff1cfd3e3f93de44985165493694c
Gregory Keeley <gregoryKeeley@pursuit.org>
2021-02-15 10:11:29 -0500

    - Imported code from prototype - Removed DSSlider

--- GoonMaker/ViewControllers/GameViewController.swift
+++ GoonMaker/ViewControllers/GameViewController.swift
@@ -6,28 +6,49 @@
 //
 
 import UIKit
-import DSSlider
 
 class GameViewController: UIViewController {
      //MARK:- IBOutlets
-    @IBOutlet weak var sliderContainer1: UIView!
-    @IBOutlet weak var sliderContainer2: UIView!
-    @IBOutlet weak var sliderContainer3: UIView!
-    @IBOutlet weak var sliderContainer4: UIView!
-    
+    @IBOutlet weak var slider1: UISlider!
+    @IBOutlet weak var slider2: UISlider!
+    @IBOutlet weak var slider3: UISlider!
+    @IBOutlet weak var slider4: UISlider!
+    @IBOutlet weak var sliderStackView: UIStackView!
     @IBOutlet weak var startTimerButton: UIButton!
     @IBOutlet weak var timerLabel: UILabel!
     @IBOutlet weak var scoreLabel: UILabel!
     
   //MARK:- Variable and Constants
     //Score
-    var slider1ScoreValue = 0
-    var slider2ScoreValue = 0
-    var slider3ScoreValue = 0
-    var slider4ScoreValue = 0
-    var currentScore = 0 {
+    var slider1MaxValue: Float = 0.0 {
         didSet {
-            scoreLabel.text = ("\(currentScore)")
+            animateSliderImage(slider: slider1)
+            checkSliderValue(slider: slider1)
+            
+        }
+    }
+    var slider2MaxValue: Float = 0.0 {
+        didSet {
+            animateSliderImage(slider: slider2)
+            checkSliderValue(slider: slider2)
+        }
+    }
+    var slider3MaxValue: Float = 0.0 {
+        didSet {
+            animateSliderImage(slider: slider3)
+            checkSliderValue(slider: slider3)
+        }
+    }
+    var slider4MaxValue: Float = 0.0 {
+        didSet {
+            animateSliderImage(slider: slider4)
+            checkSliderValue(slider: slider4)
+        }
+    }
+    
+    var currentGameScore: Float = 0.0 {
+        didSet {
+            scoreLabel.text = ("\(currentGameScore.rounded())")
         }
     }
     
@@ -60,17 +81,23 @@ class GameViewController: UIViewController {
      //MARK:- View Lifecycles
     override func viewDidLoad() {
         super.viewDidLoad()
-        setupSlider(slider: sliderContainer1)
-        setupSlider(slider: sliderContainer2)
-        setupSlider(slider: sliderContainer3)
-        setupSlider(slider: sliderContainer4)
+        setupSliders()
     }
+    override func viewDidAppear(_ animated: Bool) {
+        /*
+         NOTE: This will rotate the entire sliderStack 90 degrees, to a vertical Orientation
+        sliderStackView.transform = CGAffineTransform.init(rotationAngle: .pi/2)
+         */
+    }
+    
+     //MARK:- Functions
+
     private func reduceScore() {
-        let scores = [slider1ScoreValue, slider2ScoreValue, slider3ScoreValue, slider4ScoreValue]
+        let scores = [slider1MaxValue, slider2MaxValue, slider3MaxValue, slider4MaxValue]
         let totalScore = scores.reduce(0, { x, y in
             x + y
         })
-        currentScore = totalScore
+        currentGameScore = totalScore
     }
     private func startTimer() {
         timerIsPaused.toggle()
@@ -85,19 +112,110 @@ class GameViewController: UIViewController {
                     }
                 } else {
                     self.seconds = self.seconds + 1
-                    self.slider1ScoreValue += 1
-                    self.slider2ScoreValue += 1
-                    self.slider3ScoreValue += 1
-                    self.slider4ScoreValue += 1
-                    self.reduceScore()
+                    self.animateSliderImage(slider: self.slider1)
+                    self.animateSliderImage(slider: self.slider2)
+                    self.animateSliderImage(slider: self.slider3)
+                    self.animateSliderImage(slider: self.slider4)
                 }
             }
         } else {
             timer.invalidate()
         }
     }
+    private func animateSliderImage(slider: UISlider) {
+        let duration = 1.0
+        let climbRate: Float = 3.0
+        switch slider {
+        case slider1:
+            checkSliderValue(slider: slider1)
+            UIView.animate(withDuration: duration, animations: {
+                slider.setValue(self.slider1.value + climbRate, animated: true)
+            })
+        case slider2:
+            checkSliderValue(slider: slider2)
+            UIView.animate(withDuration: duration, animations: {
+                slider.setValue(self.slider2.value + climbRate, animated: true)
+            })
+        case slider3:
+            checkSliderValue(slider: slider3)
+            UIView.animate(withDuration: duration, animations: {
+                slider.setValue(self.slider3.value + climbRate, animated: true)
+            })
+        case slider4:
+            checkSliderValue(slider: slider4)
+            UIView.animate(withDuration: duration, animations: {
+                slider.setValue(self.slider4.value + climbRate, animated: true)
+            })
+        default:
+            return
+        }
+        self.view.layoutIfNeeded()
+        let animator = UIViewPropertyAnimator(duration: 1, curve: .easeIn) {
+        }
+        animator.startAnimation()
+    }
+    private func checkSliderValue(slider: UISlider) {
+        if slider.value == slider.maximumValue {
+            slider.isEnabled = false
+        } else {
+            slider.isEnabled = true
+        }
+    }
+     //MARK:- @IBActions
+    @objc func onSliderValChanged(slider: UISlider, event: UIEvent) {
+        var min: Float = 0.0
+        if let touchEvent = event.allTouches?.first {
+            switch touchEvent.phase {
+            case .began:
+                switch slider {
+                case slider1:
+                    if slider.value > slider1MaxValue {
+                        slider1MaxValue = slider.value
+                    }
+                case slider2:
+                    if slider.value > slider1MaxValue {
+                        slider2MaxValue = slider.value
+                    }
+                case slider3:
+                    if slider.value > slider1MaxValue {
+                        slider3MaxValue = slider.value
+                    }
+                case slider4:
+                    if slider.value > slider1MaxValue {
+                        slider4MaxValue = slider.value
+                    }
+                default:
+                    return
+                }
+            case .ended:
+                min = slider.value
+                switch slider {
+                case slider1:
+                    currentGameScore = (currentGameScore + (slider1MaxValue - min))
+                case slider2:
+                    currentGameScore = (currentGameScore + (slider2MaxValue - min))
+                case slider3:
+                    currentGameScore = (currentGameScore + (slider3MaxValue - min))
+                case slider4:
+                    currentGameScore = (currentGameScore + (slider4MaxValue - min))
+                default:
+                    return
+                }
+            default:
+                break
+            }
+        }
+    }
     
-    
+    @IBAction func resetButtonPressed(_ sender: UIButton) {
+        currentGameScore = 0
+        slider1MaxValue = 0
+        slider2MaxValue = 0
+        slider3MaxValue = 0
+        slider4MaxValue = 0
+        timerLabel.text = "00:00"
+        timer.invalidate()
+    }
     @IBAction func startButtonPressed(_ sender: UIButton) {
         startTimer()
         if timerIsPaused {
@@ -108,44 +226,59 @@ class GameViewController: UIViewController {
     }
     
 }
-extension GameViewController : DSSliderDelegate {
-    func sliderDidFinishSliding(_ slider: DSSlider, at position: DSSliderPosition) {
-        
-    }
-}
 
 
 //MARK:- Slider Setup Funcs
 extension GameViewController {
     
-    func setupSlider(slider: UIView) {
-        let slider = DSSlider(frame: slider.frame, delegate: self)
-        slider.isDoubleSideEnabled = true
-        slider.isImageViewRotating = true
-        slider.isTextChangeAnimating = true
-        slider.isDebugPrintEnabled = false
-        slider.isShowSliderText = true
-        slider.isEnabled = true
-        slider.sliderAnimationVelocity = 0.2
-        slider.sliderViewTopDistance = 0.0
-        slider.sliderImageViewTopDistance = 5
-        slider.sliderImageViewStartingDistance = 5
-        slider.sliderTextLabelLeadingDistance = 0
-        slider.sliderCornerRadius = slider.frame.height / 2
-        slider.sliderBackgroundColor = UIColor.systemGray4
-        slider.sliderBackgroundViewTextColor = DSSlider.dsSliderRedColor()
-        slider.sliderDraggedViewTextColor = DSSlider.dsSliderRedColor()
-        slider.sliderDraggedViewBackgroundColor = UIColor.gray
-        slider.sliderImageViewBackgroundColor = DSSlider.dsSliderRedColor()
-        slider.sliderTextFont = UIFont.systemFont(ofSize: 15.0)
-        //  slider.sliderImageView.image = UIImage(systemName: "arrow-icon")
-        slider.sliderBackgroundViewTextLabel.text = "SLIDE TO TURN ON!"
-        slider.sliderDraggedViewTextLabel.text = "SLIDE TO TURN OFF!"
-        
-        slider.clipsToBounds = true
-        
-        slider.sizeToFit()
-        
-        view.addSubview(slider)
+    func setupSliders() {
+        slider1.addTarget(self, action: #selector(onSliderValChanged(slider:event:)), for: .valueChanged)
+        slider2.addTarget(self, action: #selector(onSliderValChanged(slider:event:)), for: .valueChanged)
+        slider3.addTarget(self, action: #selector(onSliderValChanged(slider:event:)), for: .valueChanged)
+        slider4.addTarget(self, action: #selector(onSliderValChanged(slider:event:)), for: .valueChanged)
+    }
+}
+
+
+
+// Extension UIView from here: https://stackoverflow.com/questions/48804229/how-do-i-have-to-modify-my-uiview-extension-if-i-wanted-to-animate-the-auto-layo
+enum ConstraintType {
+    case top, leading, trailing, bottom, width, height
+}
+
+extension UIView {
+
+    func anchor(top: NSLayoutYAxisAnchor?, leading: NSLayoutXAxisAnchor?, bottom: NSLayoutYAxisAnchor?, trailing: NSLayoutXAxisAnchor?, padding: UIEdgeInsets = .zero, size: CGSize = .zero) -> [ConstraintType : NSLayoutConstraint] {
+        //translate the view's autoresizing mask into Auto Layout constraints
+        translatesAutoresizingMaskIntoConstraints = false
+
+        var constraints: [ConstraintType : NSLayoutConstraint] = [:]
+
+        if let top = top {
+            constraints[.top] = topAnchor.constraint(equalTo: top, constant: padding.top)
+        }
+
+        if let leading = leading {
+            constraints[.leading] = leadingAnchor.constraint(equalTo: leading, constant: padding.left)
+        }
+
+        if let bottom = bottom {
+            constraints[.bottom] = bottomAnchor.constraint(equalTo: bottom, constant: -padding.bottom)
+        }
+
+        if let trailing = trailing {
+            constraints[.trailing] = trailingAnchor.constraint(equalTo: trailing, constant: -padding.right)
+        }
+
+        if size.width != 0 {
+            constraints[.width] = widthAnchor.constraint(equalToConstant: size.width)
+        }
+
+        if size.height != 0 {
+            constraints[.height] = heightAnchor.constraint(equalToConstant: size.height)
+        }
+        let constraintsArray = Array<NSLayoutConstraint>(constraints.values)
+        NSLayoutConstraint.activate(constraintsArray)
+        return constraints
     }
 }
